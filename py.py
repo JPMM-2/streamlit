1c621aff6bb2f2e809e78a2aca0a933f1a44fae2
JPMM <84038900+JPMM-2@users.noreply.github.com>
2024-02-03 11:22:09 +0100

    Update py.py

--- py.py
+++ py.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
 
-st.title('NBA Player Stats Explorer')
+st.title('NBAAAA Player Stats Explorer')
 
 st.markdown("""
 This app performs simple webscraping of NBA player stats data!
@@ -17,7 +17,7 @@ st.sidebar.header('User Input Features')
 selected_year = st.sidebar.selectbox('Year', list(reversed(range(1950,2020))))
 
 # Web scraping of NBA player stats
-@st.cache_data
+#@st.cache_data
 def load_data(year):
     url = "https://www.basketball-reference.com/leagues/NBA_" + str(year) + "_per_game.html"
     html = pd.read_html(url, header = 0)
