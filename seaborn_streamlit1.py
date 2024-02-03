b295824e30b7de9b616fcb0ec4e62fad3679953e
JPMM <84038900+JPMM-2@users.noreply.github.com>
2024-02-03 12:21:32 +0100

    Update seaborn_streamlit1.py

--- seaborn_streamlit1.py
+++ seaborn_streamlit1.py
@@ -6,7 +6,7 @@ import numpy as np
 
 
 
-df0 = pd.DataFrame(np.random.randn(5000, 8), columns=("col %d" % i for i in range(20)))
+df0 = pd.DataFrame(np.random.randn(50, 8), columns=("col %d" % i for i in range(20)))
 categorical_fields = []
 numeric_fields = []
 option2 = ''
